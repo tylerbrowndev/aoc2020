db8a13d655b41011685911710cb5376799ff03f4
Tyler Brown <tbrown@simplenexus.com>
2020-12-06 23:09:01 -0700

    day 7 cleanup

--- 7.py
+++ 7.py
@@ -1,6 +1,24 @@
 f = open("input/7.txt", "r")
 lines = f.readlines()
 
+# initialize rules
+bag_rules = {}
+for line in lines:
+  key, value = line.split(' bags contain ')
+  if value[:2] == 'no':
+    bag_rules[key] = []
+    continue
+  for val in value.split(','):
+    val = val.strip()
+    num = val[0]
+    color, rest = val[1:].split('bag')
+    color = color.strip()
+    if key not in bag_rules:
+      bag_rules[key] = [(color, int(num))]
+    else:
+      bag_rules[key].append((color, int(num)))
+
+# part 1
 def find_color(color, bag):
   sub_bags = bag_rules[bag]
   for sub_bag in sub_bags:
@@ -10,6 +28,15 @@ def find_color(color, bag):
       return True
   return False
 
+count = 0
+for key in bag_rules:
+  if find_color('shiny gold', key):
+    count += 1
+print(count)
+
+
+
+# part 2
 def find_num_contains(color):
   if len(bag_rules[color]) == 0:
     return 0
@@ -23,46 +50,4 @@ def find_num_contains(color):
       count += sub_bag[1]
   return count
 
-bag_rules = {}
-for line in lines:
-  key, value = line.split(' bags contain ')
-  if value[:2] == 'no':
-    bag_rules[key] = []
-    continue
-  for val in value.split(','):
-    val = val.strip()
-    num = val[0]
-    color, rest = val[1:].split('bag')
-    color = color.strip()
-    if key not in bag_rules:
-      bag_rules[key] = [(color, int(num))]
-    else:
-      bag_rules[key].append((color, int(num)))
-
-count = 0
-for key in bag_rules:
-  if key == 'shiny gold':
-    continue
-  if find_color('shiny gold', key):
-    count += 1
-print(count)
-
-
-
-
-bag_rules = {}
-for line in lines:
-  key, value = line.split(' bags contain ')
-  if value[:2] == 'no':
-    bag_rules[key] = []
-    continue
-  for val in value.split(','):
-    val = val.strip()
-    num = val[0]
-    color, rest = val[1:].split('bag')
-    color = color.strip()
-    if key not in bag_rules:
-      bag_rules[key] = [(color, int(num))]
-    else:
-      bag_rules[key].append((color, int(num)))
 print(find_num_contains('shiny gold'))
